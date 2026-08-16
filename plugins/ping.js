40ce6968c8ae3dbdf2f48bcc414b432d22a1a78b
alexischan <alexreyd44@gmail.com>
2026-08-16 02:09:25 -0500

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -1,15 +1,7 @@
-export const desc = 'Muestra la latencia del bot'
-export const alias = ['p']
-export const cooldown = 3
-
-export default async function ping({ sock, chatId }) {
-  const inicio = Date.now()
-  const sent = await sock.sendMessage(chatId, { text: '🏓 Calculando...' })
-  const ms = Date.now() - inicio
-
-  await sock.sendMessage(
-    chatId,
-    { text: `🏓 Pong!\n> Velocidad: ${ms}ms` },
-    { quoted: sent }
-  )
+let handler = async (m, { conn }) => {
+    await conn.sendMessage(m.chat, { text: '🏓 Pong!' }, { quoted: m })
 }
+
+handler.command = ['ping']
+
+export default handler
